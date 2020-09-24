474e6313e2864e448db202f9f1b759b578b1651a
Tanvir-101 <58018158+Tanvir-101@users.noreply.github.com>
2020-09-24 20:52:23 +0600

    Add files via upload

---
+++ StatusBar.java
@@ -0,0 +1,15 @@
+
+import javax.swing.*;
+import javax.swing.border.SoftBevelBorder;
+import java.awt.*;
+
+public class StatusBar extends JPanel {
+	
+	private JLabel statusBar = new JLabel("  ");
+
+	public StatusBar() {
+		statusBar.setFont(new Font("Tahoma", Font.BOLD, 9));
+		this.add(statusBar);
+		this.setBorder(new SoftBevelBorder(javax.swing.border.BevelBorder.LOWERED));
+	}
+}
